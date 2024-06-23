2175b3252b4566553916ef51698d78bbd6570c4b
ymreddy941 <ymreddy941@gmail.com>
2024-06-23 15:10:11 +0530

    first commit

---
+++ src/main/java/com/infy/springboot_aws_exam/SpringbootAwsExamApplication.java
@@ -0,0 +1,21 @@
+package com.infy.springboot_aws_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootAwsExamApplication {
+
+	@GetMapping("/welcome")
+	public String greet(){
+		return "Welcome to aws deployment process";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/springboot_aws_exam/SpringbootAwsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.springboot_aws_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
